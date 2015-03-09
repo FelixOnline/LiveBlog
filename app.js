eaa446984911a963d439b8e9bedf4e4963bff853
Philip Kent <philip.kent@me.com>
2015-03-09 21:30:12 +0000

    Make port configurable

--- app.js
+++ app.js
@@ -97,5 +97,5 @@ function getData(url, callback) {
     });
 }
 
-app.listen(3000);
+app.listen(config.port);
 console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
--- config.sample.js
+++ config.sample.js
@@ -1,5 +1,7 @@
 var config = {};
 
 config.api = 'API_KEY';
+config.url = '';
+config.port = 3000;
 
 module.exports = config;
